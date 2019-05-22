74251910817dfe6b74b9e058efc9a3751dc6aa59
Dante <jhonalexanderfonseca1993@gmail.com>
2019-05-22 11:20:59 -0500

    Se agregan las dependencias de javascript

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        return Post::orderBy('id','DESC')->paginate();
+    }
+}
